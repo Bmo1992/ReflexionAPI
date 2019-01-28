3c22c96d752916ae3d5e0c0d7bb66a881a7ffd35
Bmo1992 <brandonseahorse@gmail.com>
2019-01-28 09:54:59 -0800

    Created new function Get-ReflexionCustomer

---
+++ Functions/Get-ReflexionCustomer.ps1
@@ -0,0 +1,42 @@
+Function Get-ReflexionCustomer
+{
+    <#
+        .SYNOPSIS
+          Return information about a Reflexion customer under a given enterprise. By default information on all customers is returned.    
+
+        .DESCRIPTION
+
+        .PARAMETER EnterpriseId
+          Specify the enterprise id of the API users enterprise.  This can be found with the Get-ReflexionApiUser function.
+
+        .EXAMPLE
+          Get-ReflexionCustomer -EnterpriseId "1234"
+
+          Returns information on all customers for the enterprise with an ID of 1234. 
+
+        .NOTES
+          NAME    : Get-ReflexionCustomer
+          AUTHOR  : BMO
+          EMAIL   : brandonseahorse@gmail.com
+          GITHUB  : github.com/Bmo1992 
+          CREATED : January 28, 2019
+    #>
+    # The enterprise ID is a required parameter and can be found with the Get-ReflexionApiUser function.
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(
+            Mandatory=$True
+        )]
+        [string]$EnterpriseId
+    )
+    
+    Try
+    {
+        Invoke-RestMethod -Uri "https://api.reflexion.net/rfx-rest-api/enterprises/$EnterpriseId" -Headers $reflexion_headers
+    }
+    Catch
+    {
+        Throw "Unable to connect to Reflexion. If your authentication token has expired run the Connect-ReflexionAPI function and try again"
+    }
+}
