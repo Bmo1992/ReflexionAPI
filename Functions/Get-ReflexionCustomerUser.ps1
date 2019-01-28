76012ce1d4bceb6707ba2027d4f5d21a65682905
Bmo1992 <brandonseahorse@gmail.com>
2019-01-28 13:42:11 -0800

    Created new function Get-ReflexionCustomerUser

---
+++ Functions/Get-ReflexionCustomerUser.ps1
@@ -0,0 +1,23 @@
+Function Get-ReflexionCustomerUser
+{
+    <#
+        .SYNOPSIS
+          Get the Reflexion information for users of a specific customer under your enterprise.
+
+        .DESCRIPTION
+
+        .EXAMPLE
+
+        .NOTES
+          NAME    : Get-ReflexionCustomerUser
+          AUTHOR  : BMO
+          EMAIL   : brandonseahorse@gmail.com
+          GITHUB  : github.com/Bmo1992
+          CREATED : January 28, 2019
+    #>
+    [CmdletBinding()]
+    Param
+    (
+    )
+
+}
