bee9ed837d3edaaf6c10220be74a40575b987a0c
Bmo1992 <brandonseahorse@gmail.com>
2019-03-08 11:39:19 -0800

    Created new function Add-ReflexionUser, tested it and confirmed it worked

---
+++ Functions/Add-ReflexionUser.ps1
@@ -0,0 +1,55 @@
+Function Add-ReflexionUser
+{
+    <#
+        .SYNOPSIS
+          Create a new user under an enterprise.
+
+        .DESCRIPTION
+
+        .PARAMETER Name
+          Specify the name of the new Reflexion user.
+
+        .PARAMETER PrimaryAddress
+          Specify the primary smtp address of the user to be created.
+
+        .PARAMETER EnterpriseId
+          Specify the enterprise Id of the enterprise the user is to be created under.
+
+        .EXAMPLE
+          Add-ReflexionUser -Name "John Doe" -PrimaryAddress "jdoe@bigcompany.com" -EnterpriseId 12345
+
+          Creates a new Reflexion mailbox with the name 'John Doe' and a primary smtp address of 'jdoe@bigcompany.com' under the enterprise with the ID of 12345. 
+
+        .NOTES
+          NAME    : Add-ReflexionUser
+          AUTHOR  : BMO
+          EMAIL   : brandonseahorse@gmail.com
+          GITHUB  : github.com/Bmo1992
+          CREATED : March 8, 2019 
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(
+            Mandatory=$True
+        )]
+        [string]$Name,
+        [string]$PrimaryAddress,
+        [string]$EnterpriseId
+    )
+
+    $new_user = @{
+        "name" = $Name;
+        "primaryAddress" = $PrimaryAddress
+    }
+
+    Try
+    {
+        Invoke-WebRequest -Uri "https://api.reflexion.net/rfx-rest-api/enterprises/$EnterpriseId/users" -Method POST `
+        -Body ($new_user | ConvertTo-Json) -Headers $reflexion_headers
+    }
+    Catch
+    {
+        Throw "Unable to connect to Reflexion or create the new user.  Please confirm that youre authenticated to the Reflexion API and that the user name or enterprise id does not already exist in Reflexion and try again"
+    }
+}
